a63ddb4ec0ab6f27fb170ff3bf5303f40aecc02f
SashaJson <sashamiller666@gmail.com>
2020-10-21 03:34:44 +0300

    added new function cmd() in app.py

--- app.py
+++ app.py
@@ -10,3 +10,22 @@ def talk(words):
 
 
 talk('Hi')
+
+
+def cmd():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('I am listening to you')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+    try:
+        task = r.recognize_google(audio).lower()
+        print('You said: ' + task)
+    except sr.UnknownValueError:
+        talk('I do not understand you')
+        task = cmd()
+
+    return task
